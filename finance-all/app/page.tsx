1efce01cccacea16d7e503d92e808ee83416d840
Eber Ortiz <ortizmas14@gmail.com>
2024-11-06 16:39:02 -0300

    seat: Implementando login com clerk

--- finance-all/app/page.tsx
+++ finance-all/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Hello World </Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
-}
+};
+
+export default Home;
