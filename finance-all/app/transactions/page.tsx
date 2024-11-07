e84308f581610fc3fb0a47375f0e7b15993d36d6
Eber Ortiz <ortizmas14@gmail.com>
2024-11-07 17:26:07 -0300

    fix: Adicionado a lista de dados

--- finance-all/app/transactions/page.tsx
+++ finance-all/app/transactions/page.tsx
@@ -1,9 +1,15 @@
-const Transactions = () => {
+import { db } from "../_lib/prisma";
+const TransactionsPage = async () => {
+    // Acessar as transações do banco de dados
+    const transactions = await db.transaction.findMany({});
+    console.log(transactions);
     return (
-        <div className="flex items-center justify-center w-screen">
-            <h1 className="text-red-500">Transaction page</h1>
+        <div  className="flex items-center justify-center w-screen">
+            {transactions.map((transaction) => (
+                <div key={transaction.id}>{transaction.name}</div>
+            ))}
         </div>
     );
 };
 
-export default Transactions;
+export default TransactionsPage;
