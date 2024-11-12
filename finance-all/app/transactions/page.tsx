614cad4e607fd2cd1d010f8d2bc1691d9040d42d
Eber Ortiz <ortizmas14@gmail.com>
2024-11-12 16:00:57 -0300

    feat: modulos: Adicionando lista de dados, componesntes, actions, etc

--- finance-all/app/transactions/page.tsx
+++ finance-all/app/transactions/page.tsx
@@ -1,14 +1,24 @@
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 const TransactionsPage = async () => {
     // Acessar as transações do banco de dados
     const transactions = await db.transaction.findMany({});
     console.log(transactions);
     return (
-        <div  className="flex items-center justify-center w-screen">
-            {transactions.map((transaction) => (
-                <div key={transaction.id}>{transaction.name}</div>
-            ))}
+        <div className="p-6 space-y-6">
+            <div  className="flex w-full items-center justify-between">
+                <h1 className="text-2xl font-bold">Transação</h1>
+                <Button className="rounded-full">
+                    Adicinar transação
+                    <ArrowDownUpIcon />
+                </Button>
+            </div>
+            <DataTable columns={transactionColumns} data={transactions} />
         </div>
+        
     );
 };
 
