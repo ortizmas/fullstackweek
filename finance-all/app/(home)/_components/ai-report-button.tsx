7ed811f8b1dbd6b38a5b3e1090dce981876cacc8
Eber Ortiz <ortizmas14@gmail.com>
2024-11-14 17:28:48 -0300

    feat: implementação de relatorio com chat gpt

--- finance-all/app/(home)/_components/ai-report-button.tsx
+++ finance-all/app/(home)/_components/ai-report-button.tsx
@@ -26,6 +26,7 @@ interface AiReportButtonProps {
 const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
   const [report, setReport] = useState<string | null>(null);
   const [reportIsLoading, setReportIsLoading] = useState(false);
+
   const handleGenerateReportClick = async () => {
     try {
       setReportIsLoading(true);
@@ -38,6 +39,7 @@ const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
       setReportIsLoading(false);
     }
   };
+
   return (
     <Dialog
       onOpenChange={(open) => {
@@ -52,6 +54,7 @@ const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
           <BotIcon />
         </Button>
       </DialogTrigger>
+
       <DialogContent className="max-w-[600px]">
         {hasPremiumPlan ? (
           <>
@@ -62,9 +65,11 @@ const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
                 sobre suas finanças.
               </DialogDescription>
             </DialogHeader>
-            <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+
+            <ScrollArea className="prose prose-h3:text-white prose-h4:text-white prose-strong:text-white max-h-[450px] text-white">
               <Markdown>{report}</Markdown>
             </ScrollArea>
+
             <DialogFooter>
               <DialogClose asChild>
                 <Button variant="ghost">Cancelar</Button>
@@ -86,6 +91,7 @@ const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
                 Você precisa de um plano premium para gerar relatórios com IA.
               </DialogDescription>
             </DialogHeader>
+
             <DialogFooter>
               <DialogClose asChild>
                 <Button variant="ghost">Cancelar</Button>
